e9bfa23cbc31229a9ac0310b3472c6fb1ea4b12a
ldbean <leighannbaxa@gmail.com>
2021-07-17 08:15:50 +0200

    navbar styling and functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,24 +2,45 @@ import logo from '../../images/logo.svg';
 import './app.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { Home, Projects, About } from '../index';
- 
+
 function App() {
   return (
-    <BrowserRouter> 
+    <BrowserRouter>
       <div className="App">
-
-        <Route exact path="/" component ={Home}/>
+        <nav className="bg-dark">
+          <ul className="nav d-flex justify-content-between">
+            <li className="nav-brand">
+              <img src={logo} className="nav-link" alt="Logo" /> 
+            </li>
+            <li class="nav-item">
+              <ul className="nav">
+                <li class="nav-item ">
+                  <Link to="/" className="nav-link">
+                    HOME
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/projects" className="nav-link">
+                    PROJECTS
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/about" className="nav-link">
+                    ABOUT
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/contact" className="nav-link">
+                    CONTACT
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
         <Route path="/projects" component={Projects} />
         <Route path="/about" component={About} />
-
-        <nav className="App-nav">
-          <img src={logo} className="logo" alt="Logo" />
-          <div className="navigation-sub">
-            <Link to="/" className="item">Home</Link>
-            <Link to="/projects" className="item">Projects</Link>
-            <Link to="/about" className="item">About</Link>
-          </div>
-        </nav>
       </div>
     </BrowserRouter>
   );
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,9 +1,34 @@
 import React from "react";
+import Thumbnail from "../thumbnail/thumbnail";
 
 function Projects(props) {
     return (
       <div className="Projects">
         <h1>Projects</h1>
+        <Thumbnail
+          link="/pierced"
+          image="https://picsum.photos/300/300"
+          title="Pierced"
+          category="Sculpts"
+        />
+        <Thumbnail
+          link="/cathedral"
+          image="https://picsum.photos/300/300"
+          title="Cathedral"
+          category="Apps"
+        />
+        <Thumbnail
+          link="/"
+          image="https://picsum.photos/300/300"
+          title="Pierced"
+          category="Sculpts"
+        />
+        <Thumbnail
+          link="/pierced"
+          image="https://picsum.photos/300/300"
+          title="Pierced"
+          category="Sculpts"
+        />
       </div>
     );
   }
