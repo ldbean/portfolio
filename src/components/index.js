b64c0c7913529728c62894fe7b6684415a417841
leighann <leighannbaxa@gmail.com>
2023-03-09 23:06:52 +0100

    fixed merges

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,6 @@
-<<<<<<< Updated upstream
-export { default as App } from './app/app';
-export { default as Home } from './home/home';
-export { default as Projects } from './projects/projects';
-export { default as About } from './about/about';
-=======
 export { default as App } from './app/app';
 export { default as Home } from './home/home';
 export { default as Projects } from './projects/projects';
 export { default as About } from './about/about';
 export { default as Contact } from './contact/contact';
->>>>>>> Stashed changes
 export { default as Footer } from './footer/footer';
