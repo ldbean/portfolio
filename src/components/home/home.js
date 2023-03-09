621016816c6e4350254dcd2d27b85a0cf73a5f63
leighann baxa <leighann@ldbean.com>
2023-03-09 23:03:32 +0100

    updated home message for under construction

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,7 @@
 function Home () {
     return (
         <div id="Home" className="">
-            <h1>HOME</h1>
+            <h3>Welcome to the home of my future Portfolio. Unfortunately, you will have to come back later. For now, please check out this archive of my old work.</h3>
         </div>
     )
 }
