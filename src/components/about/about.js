3284dbd18d8c30da35de78d699cee18ea3bf0b8a
leighann baxa <leighann@ldbean.com>
2023-03-09 22:32:51 +0100

    updated about page and started contact form

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,18 +1,28 @@
+import './about.css';
+
 function About () {
     return (
         <div id="About">
+            <div id="portrait">
+.
+            </div>
+            <div id="about-text">
             <h1>
                 About
             </h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra scelerisque sapien vel viverra. Praesent eleifend, leo sed venenatis porttitor, enim tortor pretium massa, non pharetra mi purus sit amet magna. Aliquam porttitor nunc vel orci fringilla, ac posuere libero placerat. Pellentesque viverra mollis nunc, ac posuere justo rutrum ut. Curabitur et quam enim. Quisque consectetur imperdiet lorem non tempor. Nullam at aliquam neque. Nunc condimentum leo a elit varius placerat. Nulla vehicula rutrum ornare. Proin et maximus arcu, sit amet faucibus justo. Nunc ut consectetur lectus. Sed fringilla nisi at rutrum tristique. Vestibulum rhoncus posuere ornare.
-
-            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent dolor urna, pulvinar id neque vitae, rhoncus porta lacus. Pellentesque eu tellus est. Donec dapibus, nisl ac blandit interdum, lorem diam sollicitudin mauris, non facilisis mi ex nec dui. Nulla nec tortor nec tortor pulvinar auctor efficitur ut justo. Nam consequat vestibulum mauris, quis hendrerit urna euismod ut. Vivamus quam elit, mollis at auctor non, consectetur et elit. In hac habitasse platea dictumst. Mauris non accumsan lorem. Nullam pharetra leo ante, aliquam sollicitudin mi elementum in. Nam non porttitor odio. Cras tincidunt, mi in varius eleifend, quam libero aliquet nibh, eget pellentesque neque leo id massa.
-
-            In eget tortor faucibus, tristique nisl ut, porttitor erat. Suspendisse pretium, sem nec porttitor imperdiet, elit lorem tempus arcu, non tristique lorem lorem in leo. Duis ultrices justo lectus, mollis gravida arcu mattis a. Duis imperdiet ex ac hendrerit tempor. Vivamus ac nisi tempor, consectetur nulla id, ultricies odio. In tristique dolor velit, et fermentum sem maximus ut. Nunc a dolor eu leo malesuada ultricies. Morbi ac nunc sit amet ipsum tincidunt sagittis. Curabitur ac ligula risus.
-
-            Integer bibendum lectus ac risus dictum efficitur ac aliquam lacus. Pellentesque vitae metus sit amet felis placerat interdum at sit amet tortor. Praesent at purus turpis. Cras sit amet erat sit amet dolor euismod molestie. Ut tellus lacus, consequat in neque id, malesuada vulputate justo. Mauris arcu sapien, varius ut aliquam in, lacinia quis tellus. Quisque nec lectus metus. Aliquam placerat est quis urna iaculis aliquam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque molestie pellentesque pretium. Aliquam ligula urna, congue at vehicula sit amet, maximus id mi. Aenean vel suscipit lorem. Suspendisse ut volutpat ante. Maecenas id justo lacus.
-
-            In semper mollis augue vel volutpat. Maecenas quam risus, consectetur ac sem sit amet, semper malesuada nibh. Mauris porta diam pretium est aliquet egestas. Aenean nec faucibus lacus. Mauris in mi sed erat pellentesque pellentesque et in arcu. Suspendisse in iaculis nisl. Curabitur lacinia mollis justo, eu tempor ligula varius at. Morbi semper ligula id accumsan malesuada. Duis velit velit, tempus vel egestas sit amet, mollis id magna. Proin non ullamcorper tellus. </p>
+            <p>
+                All my life, I've been told to do things a certain way by those I look up to in order to "be successful". Here's the thing, every one of those people have had something completely different to suggest for me to do. Well I know myself, and I know that choosing one option over all the other, equally delicious ones doesn't work for me. I like to do a lot of things, and I am not going to limit myself to just one. Sure that may make it difficult for someone like you, reader, who may be interested in a certain type of person to hire for your project, to filter through all the weird work I have on my portfolio in front of you today, but I like to think you've at least enjoyed your time here a little bit. And that sounds successful to me. 
+            </p>
+            <p>
+                At the end of the day, I am an artist. And as an artist, my goal is always to do whatever it takes to bring a vision to life. I love the challenge of making something out of nothing - of developing ideas and seeing them through to fruition, with all the problem solving and learning on the fly included (my favorite parts). My skill tree is widespread and always growing, not only because technologies are always evolving, but because I simply love to learn and create. Sometimes that means I jump from medium to medium.
+            </p> 
+            <p>
+                TLDR; both art and tech have been big parts of my life, so my skills and interests mostly span across the two fields. Because of this, I have picked up various disciplines over the years. Digital and traditional painting and illustration, graphic, web, and motion design, 3D game/realtime and VFX modeling, texturing, and animation, web development(I created this website with React), and even hardware and software support and repair for computers and cell phones (although that leans more towards personal interests, but has come in handy professionally).
+            </p>
+            <p>
+                If you are interested in working with me on any type of project, I would be more than happy to discuss this with you over email. Currently I my goals and interests align with Technical and Environment art for games. I hope my recent projects reflect that. 
+            </p>
+            </div>
         </div>
     );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 export { default as App } from './app/app';
 export { default as Home } from './home/home';
 export { default as Projects } from './projects/projects';
 export { default as About } from './about/about';
+=======
+export { default as App } from './app/app';
+export { default as Home } from './home/home';
+export { default as Projects } from './projects/projects';
+export { default as About } from './about/about';
+export { default as Contact } from './contact/contact';
+>>>>>>> Stashed changes
 export { default as Footer } from './footer/footer';
